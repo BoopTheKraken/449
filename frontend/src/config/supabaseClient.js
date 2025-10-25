95e0871449a6b47be6b99e95fe071880a9a814e9
David Elliott <dje0119@yahoo.com>
2025-10-25 09:14:39 -0700

    supabase Service Role key

--- frontend/src/config/supabaseClient.js
+++ frontend/src/config/supabaseClient.js
@@ -2,5 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
+const serviceRoleKey = process.env.REACT_APP_SUPABASE_SERVICE_ROLE_KEY
+
+const supabaseAdmin = createClient(supabaseUrl, serviceRoleKey)
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
